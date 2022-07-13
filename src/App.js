40acff3f53a6bd72cb336b009a9a2caeeec55947
Guadalupe Vidal <guadalupe.cruz@oracle.com>
2022-07-13 01:46:14 -0500

    add text down of image detail

--- src/App.js
+++ src/App.js
@@ -79,28 +79,30 @@ function MainPage() {
 
             <img src={imgDetail} className="img-detail" />
 
+
+            <label className="lbl-descrip-title"> Descripción del producto </label><br />
+            <p className='lbl-descrip-detail'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              It has survived not only five centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+              Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+              of Lorem Ipsum
+            </p>
+
+
           </div>
           <div className="col-md-6" >
-            <label className="lbl-sold"> Nuevo - 234 vendidos</label> <br/>
+            <label className="lbl-sold"> Nuevo - 234 vendidos</label> <br />
 
-            <label className="lbl-title-detail"> <b>Deco Reverse Sombrero Oxford</b></label><br/>
+            <label className="lbl-title-detail"> <b>Deco Reverse Sombrero Oxford</b></label><br />
             <label className="lbl-price-detail">$ 1980</label> <label className='lbl-decimals'>00</label><br />
-            <button className='btn btn-primary'> Comprar</button>
+            <button className='btn btn-primary btn-price'> Comprar</button>
           </div>
         </div>
 
-        <div className='col-md-12'>
-        <label className="lbl-descrip-title"> Descripción del producto </label><br/>
-        <p className='lbl-descrip-detail'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        It has survived not only five centuries, but also the leap into electronic typesetting, 
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
-        of Lorem Ipsum
-        </p>
-        </div>
+
 
       </div>
     </div>
